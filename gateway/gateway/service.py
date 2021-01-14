5d97cc28058d3349c7d147e62915a2ea0a684d07
Vinicius Imai <vinicius.imai@gmail.com>
2021-01-14 17:08:54 -0300

    feat: chamando microservices

--- gateway/gateway/service.py
+++ gateway/gateway/service.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from nameko.standalone.rpc import ClusterRpcProxy
+
+CONFIG = {'AMQP_URI': "amqp://guest:guest@localhost:5672"}
 
 app = FastAPI()
 
@@ -6,3 +9,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/nameko")
+async def nameko():
+    with ClusterRpcProxy(CONFIG) as rpc:
+        result = rpc.products.list()
+
+    return result
