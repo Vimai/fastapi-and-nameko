2b631781730d3d91f201b714338ff0eac2cf45ab
Vinicius Imai <vinicius.imai@gmail.com>
2021-01-20 21:44:15 -0300

    Feat: preparing to for multiples api versions

--- gateway/gateway/api/v1/router.py
+++ gateway/gateway/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from gateway.api.routes import products
+from .routes import products
 
 router = APIRouter()
 router.include_router(products.router, prefix="/products")
