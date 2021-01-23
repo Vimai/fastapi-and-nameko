ab3a7e35fccb1a45a2ab1d3ec5b9f2461157fda5
Vinicius Imai <vinicius.imai@gmail.com>
2021-01-23 20:22:59 -0300

    feat: query parameters

--- gateway/gateway/api/routes/cars.py
+++ gateway/gateway/api/routes/cars.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import APIRouter
 
@@ -11,7 +12,7 @@ class CarsBrands(str, Enum):
     honda = "honda"
 
 
-@router.get("/cars/{car_brand}")
+@router.get("/{car_brand}")
 async def get_model(car_brand: CarsBrands):
     if car_brand == CarsBrands.toyota:
         return {"car_brand": car_brand, "message": "toyota!"}
@@ -20,3 +21,11 @@ async def get_model(car_brand: CarsBrands):
         return {"car_brand": car_brand, "message": "volkswagen"}
 
     return {"car_brand": car_brand, "message": "Have some residuals"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@router.get("/")
+async def read_cars(skip: int = 0, limit: int = 10, color: Optional[str] = None):
+    return fake_items_db[skip: skip + limit]
